cafb5059f07a1a4d053201a2c56c68bad140a038
JuanS79 <42647173+JuanS79@users.noreply.github.com>
2021-02-14 18:26:01 -0300

    Update Aprendiendo_Streamlit.py

--- Aprendiendo_Streamlit.py
+++ Aprendiendo_Streamlit.py
@@ -4,9 +4,7 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
 st.title('NBA Player Stats Explorer')
-
 st.markdown("""
 This app performs simple webscraping of NBA player stats data!
 * **Python libraries:** base64, pandas, streamlit
